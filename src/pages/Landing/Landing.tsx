07a7365fb910d4f6600d1b4e181b32b3b82ee6ca
yoonho <dydgh179@gmail.com>
2023-05-05 11:16:09 +0900

    [FEAT] : 랜딩페이지 navigation 연결 및 초기 설정

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Navigation from '../../components/Navigation/Navigation';
 
 const Landing = () => {
-  return <div className="text-yellow-300">랜딩페이지</div>;
+  return (
+    <>
+      <div className="text-yellow-300">랜딩페이지</div>
+      <Navigation></Navigation>
+    </>
+  );
 };
 
 export default Landing;
