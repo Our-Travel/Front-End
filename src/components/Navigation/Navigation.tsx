4183b1337a53bdc0174595749e3dab90d2a2d12f
yoonho <dydgh179@gmail.com>
2023-05-10 22:30:57 +0900

    [feat] navigation 디자인 수정 및 코드 정리

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Outlet, useNavigate } from 'react-router-dom';
+import { Navigate, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { GrHomeRounded } from 'react-icons/gr';
 import { GrLocation } from 'react-icons/gr';
 import { TiMessages } from 'react-icons/ti';
@@ -8,8 +8,14 @@ import { BiUser } from 'react-icons/bi';
 
 const Navigation = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const path: string = location.pathname;
 
-  const home = () => {
+  function getName(currentPath: string, expectedPath: string): string {
+    return currentPath === expectedPath ? 'active' : 'navigationButton';
+  }
+
+  const main = () => {
     navigate('/main');
   };
   const chat = () => {
@@ -27,20 +33,21 @@ const Navigation = () => {
 
   return (
     <div className="absolute bg-white w-full h-14 bottom-0 z-50 box-border">
-      <ul className="flex w-full justify-around relative top-[50%] translate-y-[-50%]">
-        <li className="cursor-pointer  " onClick={home}>
-          <GrHomeRounded className="w-5 h-5 outline-orange-300" />
+      <ul className="flex w-full justify-around relative top-[50%] translate-y-[-50%] h-7">
+        {/* navigationButton 디자인속성은 index.css에 있음 */}
+        <li className={getName(path, '/main')} onClick={main}>
+          <GrHomeRounded className="w-5 h-5 " />
         </li>
-        <li className="cursor-pointer  " onClick={chat}>
+        <li className={getName(path, '/chatting')} onClick={chat}>
           <TiMessages className="w-6 h-6" />
         </li>
-        <li className="cursor-pointer  " onClick={map}>
+        <li className={getName(path, '/map')} onClick={map}>
           <GrLocation className="w-6 h-6" />
         </li>
-        <li className="cursor-pointer  " onClick={info}>
+        <li className={getName(path, '/info')} onClick={info}>
           <HiOutlineInformationCircle className="w-6 h-6" />
         </li>
-        <li className="cursor-pointer  " onClick={mypage}>
+        <li className={getName(path, '/mypage')} onClick={mypage}>
           <BiUser className="w-6 h-6" />
         </li>
       </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-color': '#FF626F',
+        'main-color2': '#FF787D',
+        'main-color3': '#FF776D',
+        'main-color4': '#FF928A',
+      },
+    },
   },
   plugins: [],
 };
