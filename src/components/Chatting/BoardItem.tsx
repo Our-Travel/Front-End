71b071222af1a2889f2acc29b2c9f981c1613803
yoonho <dydgh179@gmail.com>
2023-05-18 17:50:18 +0900

    [STYLE] boarditem 스타일 수정

--- src/components/Chatting/BoardItem.tsx
+++ src/components/Chatting/BoardItem.tsx
@@ -6,7 +6,7 @@ interface BoardItemProps {
 }
 const BoardItem = ({ nickName, content }: BoardItemProps) => {
   return (
-    <div className="relative flex h-[70px] bg-orange-200">
+    <div className="relative flex h-[70px] bg-orange-200 my-2">
       <div className="absolute top-[50%] -translate-y-[50%] bg-slate-500 w-1/5">프로필사진</div>
       <div className="absolute right-0 w-4/5 top-[50%] -translate-y-[50%] bg-gray-200 text-left px-3">
         <div className="font-semibold">{nickName}</div>
