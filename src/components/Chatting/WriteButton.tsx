f5ca522c894a05c7556128d9fa66b0fd8c69a54e
yoonho <dydgh179@gmail.com>
2023-05-19 10:04:35 +0900

    [FEAT] 게시글 작성 마크업 완성

--- src/components/Chatting/WriteButton.tsx
+++ src/components/Chatting/WriteButton.tsx
@@ -7,12 +7,15 @@ interface modal {
 }
 
 const WriteButton = ({ open, close }: modal) => {
+  const navigate = useNavigate();
   const outside = useRef<HTMLDivElement>(null);
-  const languageButton = useRef<HTMLButtonElement>(null);
-  const modalButton: { text: string }[] = [{ text: '취소하기' }, { text: '작성하기' }];
+  // const modalButton: { text: string }[] = [{ text: '취소하기' }, { text: '작성하기' }];
 
   const closeModal = () => {
     close(false);
+  };
+  const openModal = () => {
+    navigate('/board');
   };
 
   const clickOutside = (e: MouseEvent) => {
@@ -37,13 +40,17 @@ const WriteButton = ({ open, close }: modal) => {
   });
 
   return open ? (
-    <div className="fixed w-[450px] h-full top-0 bg-black bg-opacity-30 z-30" ref={outside}>
-      <div className="centerPosition relative w-64 h-36 rounded-xl text-xl overflow-hidden bg-white flex flex-col">
-        {modalButton.map(({ text }, index) => (
-          <button type="button" key={index} className="flex flex-col flex-grow items-center justify-center border-b-[1px] first:text-main-color last:border-b-0  hover:bg-gray-200" ref={languageButton}>
-            {text}
+    <div className="fixed w-[450px] h-full top-0 bg-black bg-opacity-30 z-30 text-center" ref={outside}>
+      <div className="centerPosition relative w-[300px] h-36 rounded-xl text-xl overflow-hidden bg-white flex flex-col">
+        <div className="w-full text-textbase text-lg absolute top-[20%] font-semibold">글을 작성하시겠습니까?</div>
+        <div className="flex w-full absolute bottom-2 text-lg">
+          <button className="w-1/2 mx-4 font-semibold bg-gray-200 py-2 rounded-xl" onClick={closeModal}>
+            취소하기
           </button>
-        ))}
+          <button className="w-1/2 mx-4 text-white font-extrabold bg-main-color2 py-2 rounded-xl" onClick={openModal}>
+            작성하기
+          </button>
+        </div>
       </div>
     </div>
   ) : null;
--- src/pages/Chatting/WriteBoard.tsx
+++ src/pages/Chatting/WriteBoard.tsx
@@ -3,14 +3,16 @@ import Header from '../../components/Header/Header';
 import WriteButton from '../../components/Chatting/WriteButton';
 
 const WriteBoard = () => {
-  const [WriteModalOpen, setWriteModalOpen] = useState(false);
+  const [modalOpen, setModal] = useState<boolean>(false);
+
+  const isOpen = () => setModal(!modalOpen);
 
   return (
-    <div className="relative bg-slate-300 h-[100vh]">
+    <div className="relative h-[100vh]">
       <Header title="게시글 작성" back={true} icon={''} />
-      {WriteModalOpen && <WriteButton open={setWriteModalOpen} close={''} />}
+      {modalOpen && <WriteButton open={modalOpen} close={setModal} />}
       <textarea name="content" placeholder="일정 / 장소를 이야기하면 동행을 더 빨리 찾을 수 있어요!" className="w-[90%] h-[50vh] text-sm lg:text-base mt-4 px-4 py-4  border rounded-xl border-slate-400" />
-      <button className="absolute bottom-[80px] w-[90%] left-[50%] -translate-x-[50%] bg-main-color py-3 rounded-xl text-white text-lg font-extrabold" onClick={showWriteModal}>
+      <button className="absolute bottom-[80px] w-[90%] left-[50%] -translate-x-[50%] bg-main-color py-3 rounded-xl text-white text-lg font-extrabold" onClick={isOpen}>
         작성하기
       </button>
     </div>
