36481be098cb3244cbe887c9a3d6eb333aed47c9
oweaj <qwe1520156@naver.com>
2023-05-31 15:05:45 +0900

    [FEAT] modal 컴포넌트 각 페이지에 맞춰 적용

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,49 +1,56 @@
-import { useRef, useEffect, Dispatch, SetStateAction } from 'react';
+import React, { useRef, useEffect, Dispatch, SetStateAction } from 'react';
 
 interface modal {
   open: boolean;
   close: Dispatch<SetStateAction<boolean>>;
+  data: dataProps[];
+  page: string;
 }
-const modalButton: { text: string }[] = [{ text: '한국어 (South Korea)' }, { text: 'English' }];
 
-export default function Modal({ open, close }: modal) {
+interface dataProps {
+  text: string;
+}
+
+const Modal = ({ open, close, data, page }: modal) => {
   const outside = useRef<HTMLDivElement>(null);
-  const languageButton = useRef<HTMLButtonElement>(null);
 
   const closeModal = () => {
     close(false);
   };
 
-  const clickOutside = (e: MouseEvent) => {
-    if (outside.current === e.target) {
-      closeModal();
-    }
-  };
-
-  const escClose = (e: KeyboardEvent) => {
-    if (e.key === 'Escape') {
+  const clickCancel: any = (e: React.MouseEvent<HTMLDivElement> & React.KeyboardEvent<HTMLButtonElement>) => {
+    if (outside.current === e.target || e.key === 'Escape') {
       closeModal();
     }
   };
 
   useEffect(() => {
-    document.addEventListener('click', clickOutside);
-    document.addEventListener('keydown', escClose);
+    document.addEventListener('click', clickCancel);
+    document.addEventListener('keydown', clickCancel);
     return () => {
-      document.removeEventListener('click', clickOutside);
-      document.removeEventListener('keydown', escClose);
+      document.removeEventListener('click', clickCancel);
+      document.removeEventListener('keydown', clickCancel);
     };
   });
 
   return open ? (
     <div className="fixed w-[450px] h-full top-0 bg-black bg-opacity-30 z-30" ref={outside}>
-      <div className="centerPosition relative w-64 h-32 rounded-xl text-xl overflow-hidden bg-white flex flex-col">
-        {modalButton.map(({ text }, index) => (
-          <button type="button" key={index} className="flex flex-col flex-grow items-center justify-center line after:absolute after:top-1/2 last:after:content-none hover:bg-gray-200" ref={languageButton}>
+      <div className="centerPosition relative w-64 h-36 rounded-xl text-xl overflow-hidden bg-white flex flex-col">
+        {data.map(({ text }, index) => (
+          <button
+            type="button"
+            key={index}
+            className={`flex flex-col flex-grow items-center justify-center line after:absolute after:top-1/2 last:after:content-none hover:bg-gray-200 + ${page === 'mypage' ? 'last:text-check-red' : ''}`}
+            onClick={() => {
+              text === '취소' ? closeModal() : null;
+            }}
+          >
             {text}
           </button>
         ))}
       </div>
     </div>
   ) : null;
-}
+};
+
+export default Modal;
