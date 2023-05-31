2411d55921f6c6c516f60c05822a2d00a4c12aec
oweaj <qwe1520156@naver.com>
2023-05-31 15:02:31 +0900

    [FEAT] 회원가입 api 연동

--- src/components/LoginButton/LoginButton.tsx
+++ src/components/LoginButton/LoginButton.tsx
@@ -1,27 +1,36 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 interface buttonInfo {
   name: string;
   page: boolean;
+  active: boolean;
+  success: number;
+  // Test용
+  click: (() => Promise<void>) | any;
 }
 
-export function LoginButton({ name, page }: buttonInfo) {
-  // 나중에 Link 태그 지워야함
+export function LoginButton({ name, page, active, success, click }: buttonInfo) {
+  const navigate = useNavigate();
+
+  const clickMove = () => {
+    if (page || (name === '가입하기' && active && success === 200)) {
+      navigate('/signin');
+    } else if (name === '로그인' && active) {
+      navigate('/main');
+    }
+  };
+
   return (
-    <Link to="/signin">
-      <button type="button" className={page ? 'buttonStyle' : 'noActiveButton'}>
-        {name}
-      </button>
-    </Link>
+    <button type="submit" className={page || active ? 'buttonStyle' : 'noActiveButton'} onClick={clickMove} disabled={page ? active : !active}>
+      {name}
+    </button>
   );
 }
 
 export function LoginKakao() {
   return (
-    <Link to="/signin">
-      <button type="button" className="buttonStyle">
-        <img src="/assets/kakaoLogin.svg" alt="카카오계정으로 로그인" />
-      </button>
-    </Link>
+    <button type="button" className="buttonStyle">
+      <img src="/assets/kakaoLogin.svg" alt="카카오계정으로 로그인" />
+    </button>
   );
 }
