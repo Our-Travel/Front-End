77ca5308aa9327edde3e1b8c68307c3a68d6deaa
Sumin Kim <ksm6399@gmail.com>
2023-06-12 16:50:01 +0900

    [STYLE] 스타일 수정

--- src/components/Chatting/BoardList.tsx
+++ src/components/Chatting/BoardList.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import BoardItem from './BoardItem';
 import { Link } from 'react-router-dom';
 import Chatting from './../../pages/Chatting/Chatting';
+import PostModal from '../../components/Modal/PostModal';
 
 // BoardList는 게시판 리스트가 있는 컴포넌트로
 // BoardItem 들을 호출해오는 곳입니다
@@ -12,13 +13,17 @@ const data = [
   { nickName: 'test3', content: 'content3' },
 ];
 
+const [modalOpen, setModal] = useState<boolean>(false);
+
+const isOpen = () => setModal(!modalOpen);
+
 const BoardList = () => {
   return (
     <div>
       {data.map(({ nickName, content }, index) => (
-        <Link to={'/board/chatting'} key={index}>
-          <BoardItem key={index} nickName={nickName} content={content} />
-        </Link>
+        // <Link to={'/board/chatting'} key={index}>
+        <BoardItem key={index} nickName={nickName} content={content} />
+        // </Link>
       ))}
     </div>
   );
--- src/pages/Chatting/Board.tsx
+++ src/pages/Chatting/Board.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
 import Header from '../../components/Header/Header';
 import BoardList from '../../components/Chatting/BoardList';
 import { BsFillChatDotsFill } from 'react-icons/bs';
+import React, { useState, useRef } from 'react';
+import PostModal from '../../components/Modal/PostModal';
 
 const Board = () => {
+  const [modalOpen, setModal] = useState<boolean>(false);
+
+  const isOpen = () => setModal(!modalOpen);
+
   const icon = <BsFillChatDotsFill />;
   return (
     <div>
