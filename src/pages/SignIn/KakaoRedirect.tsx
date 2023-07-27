12e063f3bbb4620551410b384d3855aa25d7f50c
oweaj <qwe1520156@naver.com>
2023-07-27 14:29:48 +0900

    [CHORE] 카카오 로그인 진행중

--- src/pages/SignIn/KakaoRedirect.tsx
+++ src/pages/SignIn/KakaoRedirect.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { useSetRecoilState } from 'recoil';
@@ -11,53 +11,59 @@ const KakaoRedirect = () => {
   const navigate = useNavigate();
   const setToken = useSetRecoilState(token);
 
+  // useEffect(() => {
+  const code = new URL(window.location.href).searchParams.get('code');
+  const grantType = 'authorization_code';
+  const KAKAO_KEY = process.env.REACT_APP_REST_API_KEY;
+  // const REDIRECT_URI = `${process.env.REACT_APP_REST_API_SERVER}/login/oauth2/code/kakao`;
+  const REDIRECT_URI = 'http://localhost:3000/kakao/redirect';
+
   useEffect(() => {
-    const code = new URL(window.location.href).searchParams.get('code');
-    const grantType = 'authorization_code';
-    const KAKAO_KEY = process.env.REACT_APP_REST_API_KEY;
-    // const REDIRECT_URI = 'http://localhost:8080/oauth2/kakao';
-    const REDIRECT_URI = 'http://localhost:3000/kakao/redirect';
-
     axios
-      .post(
-        `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${KAKAO_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`,
-        {},
-        {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-          },
-        }
-      )
+      .get(`${process.env.REACT_APP_REST_API_SERVER}/oauth2/authorization/kakao`)
       .then((res) => {
-        const { access_token } = res.data;
-        axios
-          .post(
-            'https://kapi.kakao.com/v2/user/me',
-            {},
-            {
-              headers: {
-                Authorization: `Bearer ${access_token}`,
-                'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
-              },
-            }
-          )
-
-          .then((res) => {
-            // setInfo({ email: res.data.kakao_account.email, password: { data: '', state: false }, nickName: res.data.kakao_account.profile.nickname, pwCheck: { data: '', state: false } });
-            setToken(access_token);
-            localStorage.setItem('token', access_token);
-            navigate('/main');
-            alert('✅ 환영합니다. 좋은하루 되세요. ✅'); // 완성되면 msg로 문구 변경
-          })
-          .then(() => {
-            console.log('성공');
-          });
+        console.log(res);
       })
-      .catch(() => {
-        navigate('/signin');
-        alert('데이터를 받아오는 과정에 문제가 생겼습니다.😹');
+      .catch((error) => {
+        console.log(error);
       });
   }, []);
+
+  //   axios
+  //     .post(`https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${KAKAO_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`, {
+  //       headers: {
+  //         'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+  //       },
+  //     })
+  //     .then((res) => {
+  //       const { access_token } = res.data;
+  //       axios
+  //         .post(
+  //           'https://kapi.kakao.com/v2/user/me',
+  //           {},
+  //           {
+  //             headers: {
+  //               Authorization: `Bearer ${access_token}`,
+  //               'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+  //             },
+  //           }
+  //         )
+  //         .then((res) => {
+  //           // setInfo({ email: res.data.kakao_account.email, password: { data: '', state: false }, nickName: res.data.kakao_account.profile.nickname, pwCheck: { data: '', state: false } });
+  //           setToken(access_token);
+  //           localStorage.setItem('token', access_token);
+  //           navigate('/main');
+  //           alert('✅ 환영합니다. 좋은하루 되세요. ✅'); // 완성되면 msg로 문구 변경
+  //         })
+  //         .then(() => {
+  //           console.log('성공');
+  //         });
+  //     })
+  //     .catch(() => {
+  //       navigate('/signin');
+  //       alert('데이터를 받아오는 과정에 문제가 생겼습니다.😹');
+  //     });
+  // }, []);
 
   return <></>;
 };
