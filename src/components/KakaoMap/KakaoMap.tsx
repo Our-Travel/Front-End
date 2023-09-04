67b7d9dd7e9d8538e5abf265a614e1f684f55202
Sumin Kim <ksm6399@gmail.com>
2023-09-04 21:24:01 +0900

    [FEAT] 지도 미완성

--- src/components/KakaoMap/KakaoMap.tsx
+++ src/components/KakaoMap/KakaoMap.tsx
@@ -1,6 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useGeolocation from '../../hooks/useGeolocation';
 import Spinner from '../../shared/Spinner';
+import TourModal from '../TouristList/TourModal';
+import axios from 'axios';
+import { aroundLoc, accommodation } from '../../Atom/atom';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 
 declare global {
   interface Window {
@@ -8,10 +12,67 @@ declare global {
   }
 }
 
-const KakaoMap = () => {
-  const location = useGeolocation();
+interface MapProps {
+  token: string;
+}
+
+interface Place {
+  address_name: string;
+  distance: number;
+  id: number;
+  phone: string;
+  place_name: string;
+  place_url: string;
+  road_address_name: string;
+  x: number;
+  y: number;
+}
+
+const KakaoMap = ({ token }: MapProps) => {
   const [map, setMap] = useState<any>();
   const [marker, setMarker] = useState<any>();
+
+  const location = useGeolocation();
+  // recoil
+  const [mapToggle, setMapToggle] = useState<boolean>(false);
+  const [aroundPlace, setAroundPlace] = useRecoilState(aroundLoc);
+  const setCcommo = useSetRecoilState(accommodation);
+  const [selectPost, setSelectPost] = useState<Place | null>(null);
+  const handleMapToggle = (place: Place) => {
+    setSelectPost(place);
+    setMapToggle(!mapToggle);
+  };
+
+  useEffect(() => {
+    if (location.loaded && location.coordinates) {
+      const resAroundLoc = axios.get(`http://localhost:8080/api/local-place/spot?latitude=${location.coordinates?.lat}&longitude=${location.coordinates?.lng}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const resAccommodation = axios.get(`http://localhost:8080/api/local-place/hotel?latitude=${location.coordinates?.lat}&longitude=${location.coordinates?.lng}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      axios
+        .all([resAroundLoc, resAccommodation])
+        .then(
+          axios.spread((res1, res2) => {
+            // 첫 번째 요청 결과: res1.data
+            // 두 번째 요청 결과: res2.data
+            setAroundPlace(res1.data.data.documents);
+            setCcommo(res2.data.data.documents);
+            console.log(res2.data.data.documents);
+          })
+        )
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [location.loaded]);
 
   useEffect(() => {
     window.kakao.maps.load(() => {
--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -10,7 +10,7 @@ const Map = ({ token }: MapProps) => {
     <div className="h-screen">
       <h1>map페이지</h1>
       <div className="p-1">
-        <KakaoMap />
+        <KakaoMap token={token} />
       </div>
     </div>
   );
