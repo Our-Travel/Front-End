ee5273a23425c0f4a4ec030a3b90ef7244dddfe4
oweaj <qwe1520156@naver.com>
2023-09-08 14:03:51 +0900

    [CHORE] 좌표위치 이동 및 text 수정

--- src/components/Main/KoreaMapChart.tsx
+++ src/components/Main/KoreaMapChart.tsx
@@ -4,10 +4,10 @@ import { GrFormLocation } from 'react-icons/gr';
 const KoreaMapChart = () => {
   return (
     <>
-      <div className="w-full text-left px-2 text-base 2xl:text-lg 2xl:mt-4 transition-all duration-500">
+      <div className="w-full text-left text-base px-2 2xl:text-lg 2xl:mt-4 transition-all duration-500">
         <GrFormLocation className="w-8 h-8 inline-block font-normal" />
         <span className="font-semibold text-gray-600">
-          해당 <b className="text-main-color">지역을 클릭해</b> Host를 찾아보세요.
+          해당 <b className="text-main-color">지역의 등록된 Host 수</b>를 참고해 클릭해보세요.
         </span>
       </div>
       <KoreaMap />
