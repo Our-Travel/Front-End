ca38c70b4f50be51690703ef2644298d5e9a798b
oweaj <qwe1520156@naver.com>
2023-09-08 14:06:40 +0900

    [FEAT] 등록된 host 클릭시 roomId atom 생성

--- src/Atom/userAtom.ts
+++ src/Atom/userAtom.ts
@@ -1,6 +1,14 @@
 import { atom } from 'recoil';
+// import { recoilPersist } from 'recoil-persist';
+
+// const { persistAtom } = recoilPersist();
 
 export const hostCheck = atom<boolean>({
   key: 'hostCheck',
   default: false,
+});
+
+export const hostRoomId = atom<number>({
+  key: 'hostRoomId',
+  default: 0,
 });
