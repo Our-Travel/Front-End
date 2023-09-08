0a2c48e4b25607b772f4cf0d24166a3a3966580a
oweaj <qwe1520156@naver.com>
2023-09-08 14:02:19 +0900

    [FEAT] 클릭시 채팅방 유무에 따른 생성 및 입장

--- src/pages/Main/HostList.tsx
+++ src/pages/Main/HostList.tsx
@@ -5,6 +5,8 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import Spinner from '../../shared/Spinner';
 import { AiOutlineConsoleSql } from 'react-icons/ai';
+import { useRecoilState } from 'recoil';
+import { hostRoomId } from 'Atom/userAtom';
 
 interface listInfo {
   member_id: number;
@@ -15,45 +17,62 @@ interface listInfo {
 
 const HostList = () => {
   const [hosts, setHosts] = useState<listInfo[]>([]);
+  const [roomCheck, setRoomCheck] = useState<boolean>(false);
   const [hostMsg, setHostMsg] = useState<string>('');
   const [numberId, setNumberId] = useState<number | null>(null);
+  const [roomId, setRoomId] = useRecoilState(hostRoomId);
   const { regionCode, regionName } = useParams();
   const navigate = useNavigate();
+  const config = {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  };
 
   useEffect(() => {
     const hostData = async () => {
       try {
         const url = `${process.env.REACT_APP_REST_API_SERVER}/hosts/list?regionCode=${regionCode}`;
-        const response = await axios.get(url);
+        const response = await axios.get(url, config);
         console.log(response);
         setHosts(response.data.data);
+        response.data.data.forEach((el: any) => setRoomCheck(el.chat_room_exist));
       } catch (error) {
         if (axios.isAxiosError(error)) setHostMsg(error.response?.data.msg);
       }
     };
     hostData();
-  }, []);
+  }, [roomCheck]);
 
-  // 채팅방 생성 및 입장
+  // 채팅방 유무에 따른 생성 및 입장
   const hostChatting = async (memberId: number) => {
-    const url = `${process.env.REACT_APP_REST_API_SERVER}/room`;
-    const config = {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    };
-
     try {
-      // 채팅방 생성 post통신
-      const createRes = await axios.post(`${url}/host/${memberId}`, {}, config);
-      const roomId = createRes.data.data.chat_room_id;
-      console.log(createRes);
+      const url = `${process.env.REACT_APP_REST_API_SERVER}/room/host/${memberId}`;
+      // 기존에 채팅방이 없으면 생성 post, 있으면 채팅방 입장
+      if (!roomCheck) {
+        const createRes = await axios.post(url, {}, config);
+        console.log(createRes);
+        chatEnter(createRes.data.data.chat_room_id);
+        setRoomId(createRes.data.data.chat_room_id);
+      } else {
+        console.log(roomId);
+        chatEnter(roomId);
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        alert(error.response?.data.msg);
+      }
+    }
+  };
 
-      // 채팅방이 생성되면 채팅방 입장 get통신
-      const enterRes = await axios.get(`${url}/${roomId}`, config);
-      alert(enterRes.data.msg);
-      navigate(`/chatting/${roomId}`);
-      console.log(enterRes);
+  // 채팅방 입장 get
+  const chatEnter = async (room_id: number) => {
+    try {
+      const url = `${process.env.REACT_APP_REST_API_SERVER}/room/${room_id}`;
+      const response = await axios.get(url, config);
+      alert(response.data.msg);
+      navigate(`/chatting/${room_id}`);
+      console.log(response);
     } catch (error) {
       if (axios.isAxiosError(error)) {
         alert(error.response?.data.msg);
@@ -79,6 +98,7 @@ const HostList = () => {
                 <span>{list.nick_name}</span>
                 <div className={`${list.member_id === numberId && 'h-24'} text-main-color`}>
                   <span className={`line-clamp-2 ${list.member_id === numberId && 'block'}`}>{list.hash_tag}</span>
+                  {/* hash_tag 생략해도 너무 많아질 경우 레이아웃 깨지는 관계로 #의 개수 및 글자 수 제한 지정해보기  */}
                   {list.hash_tag.length > 23 && (
                     <button type="button" id={list.nick_name} className="text-gray-600" onClick={() => idCheck(list.member_id)}>
                       {list.member_id === numberId ? '닫기' : '더보기'}
