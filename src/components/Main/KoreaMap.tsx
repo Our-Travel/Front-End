9ae5ad050dfa24c47f7ec074937cabee4090e475
oweaj <qwe1520156@naver.com>
2023-09-08 14:06:00 +0900

    [FEAT] 지역별 등록된 host 수 map에 count 표시

--- src/components/Main/KoreaMap.tsx
+++ src/components/Main/KoreaMap.tsx
@@ -1,47 +1,67 @@
-import React, { MouseEvent, useState } from 'react';
+import axios from 'axios';
+import React, { MouseEvent, KeyboardEvent, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import mapData from './mapData';
 import mapText from './mapText';
 
+interface hostMapData {
+  region_code: number;
+  count: number;
+}
+
 const KoreaMap = () => {
   const navigate = useNavigate();
-  const [textMouse, setTextMouse] = useState<boolean>(false);
-  const [textName, setTextName] = useState<string | null>('');
+  const [hostMap, setHostMap] = useState<hostMapData[]>([]);
 
-  const handleLocationClick = (e: MouseEvent<SVGPathElement | SVGTextElement>) => {
-    const target = e.target as SVGPathElement | SVGTextElement;
-    if (target.tagName === 'path' || target.tagName === 'tspan') {
+  const handleLocation = (e: MouseEvent<SVGElement> | KeyboardEvent<SVGElement>) => {
+    const mouse = e as MouseEvent<SVGElement>;
+    const keyboard = e as KeyboardEvent<SVGElement>;
+    const mouseItem = mouse.target as SVGElement;
+
+    if ((e.type === 'click' && (mouseItem.tagName === 'path' || mouseItem.tagName === 'tspan')) || keyboard.key === 'Enter') {
       navigate('/main/selectLocation');
     }
   };
 
-  const handleMouseEnter = (e: MouseEvent<SVGTextElement>) => {
-    const target = e.target as SVGTextElement;
-    setTextName(target.textContent);
-    setTextMouse(true);
-  };
+  useEffect(() => {
+    const hostDataCount = async () => {
+      try {
+        const url = 'https://ourtravel.site/api/dev/hosts/map';
+        const response = await axios.get(url, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } });
+        setHostMap(response.data.data);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          alert(error.response?.data.msg);
+        }
+      }
+    };
+    hostDataCount();
+  }, []);
 
-  const handleMouseLeave = () => {
-    setTextName(null);
-    setTextMouse(false);
+  // 지역코드와 매개변수로 받은 code가 맞으면 해당 지역 등록된 host count 표시
+  const hostMapData = (code: number) => {
+    for (let data of hostMap) {
+      if (data.region_code === code) return data.count;
+    }
   };
 
   return (
     <div className="w-full h-full">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox={mapData.viewBox} className="svgStyle svgBig transition-all duration-500" role="group" aria-label={mapData.label}>
-        <g role="group" onClick={handleLocationClick}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox={mapData.viewBox} className="svgStyle svgBig" role="group" aria-label={mapData.label}>
+        <g role="group" onClick={handleLocation}>
           <g>
             {mapData.locations.map((location) => (
-              <g key={location.id} id={location.id} focusable={true} className={textMouse && textName === location.name ? 'fill-red-300' : 'pathStyle'}>
+              <g key={location.id} id={location.id} focusable={true} className="pathStyle" onKeyDown={handleLocation}>
                 <path d={location.path} aria-label={location.name} tabIndex={0} className="focus:fill-red-300 outline-none" />
               </g>
             ))}
           </g>
           <g>
             {mapText.mapInfo.map((item) => (
-              <g key={item.name} className="fill-black stroke-0 cursor-pointer outline-none" transform={`translate(${item.x}, ${item.y})`}>
-                <text onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+              <g key={item.name} className="fill-black stroke-0 text-2xl cursor-pointer outline-none" transform={`translate(${item.x}, ${item.y})`}>
+                <text className="hover:fill-main-color hover:scale-125">
                   <tspan>{item.name}</tspan>
+                  {hostMapData(item.value)}
                 </text>
               </g>
             ))}
