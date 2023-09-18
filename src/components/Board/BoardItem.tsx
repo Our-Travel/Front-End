7e8987e0cf2953b181b14f8ea22fede3c34ac84c
yoonho <dydgh179@gmail.com>
2023-09-18 15:58:21 +0900

    [fix] 게시글 아이디 글넘침 수정

--- src/components/Board/BoardItem.tsx
+++ src/components/Board/BoardItem.tsx
@@ -22,7 +22,7 @@ const BoardItem = ({ writer, title, content, like_counts, onItemClick }: BoardIt
 
   return (
     <div onClick={handleClick} onKeyDown={handleKeyDown} tabIndex={0} className="relative flex h-[80px] hover:bg-pink-50 border-b-2 pb-24 px-2">
-      <div className="absolute top-[50%] -translate-y-[50%] w-1/5 px-2 overflow-hidd whitespace-nowrap">
+      <div className="absolute top-[50%] -translate-y-[50%] w-1/5 px-2 overflow-hidden whitespace-nowrap">
         <p className="w-full font-bold text-ellipsis">{writer}</p>
       </div>
       <div className="absolute left-1/4 w-4/5 top-[50%] -translate-y-[50%] text-left px-3">
