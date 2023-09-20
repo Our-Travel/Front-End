9269a7557f9a14ad319a30e7beb1e98fa33e82c9
dydgh142 <dydgh142@naver.com>
2023-09-20 15:32:53 +0900

    [fix] 메인페이지 스와이퍼 앞뒤 버튼 제거

--- src/components/Main/Banner.tsx
+++ src/components/Main/Banner.tsx
@@ -1,7 +1,6 @@
 import { useRef } from 'react';
 import SwiperCore, { Autoplay, EffectFade, Navigation, Pagination, Scrollbar } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { GrPrevious, GrNext } from 'react-icons/gr';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-fade';
@@ -15,23 +14,10 @@ import mokpo from '../../assets/udon.png';
 const Banner = () => {
   SwiperCore.use([Navigation, Scrollbar]);
 
-  const prevRef = useRef<HTMLButtonElement>(null);
-  const nextRef = useRef<HTMLButtonElement>(null);
-
   const items = [{ src: test }, { src: forest }, { src: mokpo }];
 
   const navigationSettings = {
-    navigation: {
-      prevEl: prevRef.current, // 이전 버튼
-      nextEl: nextRef.current, // 다음 버튼
-    },
     onBeforeInit: (swiper: SwiperCore) => {
-      if (typeof swiper.params.navigation !== 'boolean') {
-        if (swiper.params.navigation) {
-          swiper.params.navigation.prevEl = prevRef.current;
-          swiper.params.navigation.nextEl = nextRef.current;
-        }
-      }
       swiper.navigation.update();
     },
   };
@@ -59,12 +45,6 @@ const Banner = () => {
           );
         })}
       </Swiper>
-      <button ref={prevRef} className="w-[25px] h-[25px] border-[1px] border-slate-500 border-opacity-30 rounded-2xl relative bottom-16 z-10 right-44  bg-slate-300 bg-opacity-30 ">
-        <GrPrevious className="w-[15px] h-[15px]  m-auto" />
-      </button>
-      <button ref={nextRef} className="w-[25px] h-[25px] border-[1px] border-slate-500 border-opacity-30 rounded-2xl relative bottom-16 z-10 left-44 bg-slate-300 bg-opacity-30">
-        <GrNext className="w-[15px] h-[15px] m-auto " />
-      </button>
     </>
   );
 };
