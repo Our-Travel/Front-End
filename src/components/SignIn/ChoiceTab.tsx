6bf6b6754ef518f97047f259416cf10a3879bfd3
dydgh142 <dydgh142@naver.com>
2023-09-20 14:45:05 +0900

    [fix] 피드백 모달창 외부영역클릭시 모달창 닫기 수정

--- src/components/SignIn/ChoiceTab.tsx
+++ src/components/SignIn/ChoiceTab.tsx
@@ -8,20 +8,22 @@ const ChoiceTab = () => {
     setModal(true);
   };
   return (
-    <div className="flex felx-row items-center justify-center my-6 text-gray-500">
+    <>
       {modal && <Feedback setModal={setModal} />}
-      <button className="text-gray-500" onClick={openModal}>
-        feedback
-      </button>
-      <span className="mx-5">|</span>
-      <Link to="/signup" className="px-3">
-        <span>회원가입</span>
-      </Link>
-      <span className="mx-5">|</span>
-      <Link to="/main" className="px-3">
-        <span>둘러보기</span>
-      </Link>
-    </div>
+      <div className="flex felx-row items-center justify-center my-6 text-gray-500">
+        <button className="text-gray-500" onClick={openModal}>
+          feedback
+        </button>
+        <span className="mx-5">|</span>
+        <Link to="/signup" className="px-3">
+          <span>회원가입</span>
+        </Link>
+        <span className="mx-5">|</span>
+        <Link to="/main" className="px-3">
+          <span>둘러보기</span>
+        </Link>
+      </div>
+    </>
   );
 };
 
--- src/components/SignIn/Feedback.tsx
+++ src/components/SignIn/Feedback.tsx
@@ -9,13 +9,16 @@ const Feedback = ({ setModal }: Props) => {
     setModal(false);
   };
   return (
-    <div className="absolute top-1/2 -translate-y-1/2 w-[400px] h-[500px] z-20 bg-pink-50 rounded-lg">
-      <h3 className="font-semibold text-gray-800 my-4 text-lg">여러분의 소중한 의견에 감사드립니다.</h3>
-      <textarea className="w-[350px] h-[350px] py-2 px-3" placeholder="소중한 의견을 적어주세요"></textarea>
-      <button className="buttonStyle buttonHoverSize text-white font-bold w-[300px]" onClick={closeModal}>
-        보내기
-      </button>
-    </div>
+    <>
+      <div onClick={closeModal} className="absolute w-full h-screen modalPosition bg-gray-400 opacity-25" />
+      <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-[400px] h-[500px] z-20 bg-pink-50 rounded-lg">
+        <h3 className="font-semibold text-gray-800 my-4 text-lg">여러분의 소중한 의견에 감사드립니다.</h3>
+        <textarea className="w-[350px] h-[350px] py-2 px-3" placeholder="소중한 의견을 적어주세요"></textarea>
+        <button className="buttonStyle buttonHoverSize text-white font-bold w-[300px]" onClick={closeModal}>
+          보내기
+        </button>
+      </div>
+    </>
   );
 };
 
