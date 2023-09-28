d106846f038267240d3f31b98b54d92707b15cc2
yoonho <dydgh179@gmail.com>
2023-09-28 17:31:37 +0900

    [fix] 프로필사진 경로 받아오기 수정

--- src/components/Board/BoardItem.tsx
+++ src/components/Board/BoardItem.tsx
@@ -26,7 +26,7 @@ const BoardItem = ({ image_path, writer, title, like_counts, onItemClick }: Boar
   return (
     <div onClick={handleClick} onKeyDown={handleKeyDown} tabIndex={0} className="relative flex h-[80px] hover:bg-pink-50 border-b-2 pb-24 px-2">
       <div className="absolute top-[50%] -translate-y-[50%] w-1/5 px-2 overflow-hidden whitespace-nowrap">
-        <img src={image_path || (signType ? '/assets/profile.svg' : '/assets/profileSocial.svg')} className="w-30 h-30 rounded-full border border-gray-300" alt="마이페이지 프로필사진" />
+        <img src={image_path || (signType ? '/assets/profileSocial.svg' : image_path)} className="w-30 h-30 rounded-full border border-gray-300" alt="마이페이지 프로필사진" />
       </div>
       <div className="absolute left-1/4 w-4/5 top-[50%] -translate-y-[50%] text-left px-3">
         <div className="font-semibold mr-10">{writer}</div>
