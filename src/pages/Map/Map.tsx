1889a5613cad17e795469383ec92da117a714da0
yoonho <dydgh179@gmail.com>
2023-10-16 22:36:06 +0900

    [style] 지도 필터링 글씨크기 변경

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -20,7 +20,7 @@ const Map = () => {
         {contentTypes.map((contentType) => (
           <button
             key={contentType.key}
-            className={`text-base border rounded-xl my-1 mx-[2px] p-1 h-8 hover:bg-pink-50 ${selectedButtonIndex === contentType.value ? 'bg-gray-500 text-white font-bold' : ''}`}
+            className={`text-[clamp(10px,3vw,16px)] border rounded-xl my-1 mx-[2px] p-1 h-8 hover:bg-pink-50 ${selectedButtonIndex === contentType.value ? 'bg-gray-500 text-white font-bold' : ''}`}
             onClick={() => handleButtonClick(contentType.value)}
           >
             {m(contentType.key)}
