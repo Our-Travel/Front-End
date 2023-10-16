2baa4112abaf11c1a764c4b9744809a09d479f49
yoonho <dydgh179@gmail.com>
2023-10-16 10:31:44 +0900

    [style] 게시글 필터링영역 레이아웃 수정

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -26,10 +26,13 @@ const Board = () => {
     <div>
       <Header title={'여행친구 구하기'} back={false} icon={icon} />
       <div className="relative border-b-2 px-3 flex shrink-0">
-        <Swiper slidesPerView={5.5} spaceBetween={10} keyboard={true} className="mySwiper h-12 mr-10 relative">
+        <Swiper slidesPerView={5.2} spaceBetween={10} keyboard={true} className="mySwiper h-12 mr-10 relative">
           {regions.map((region) => (
             <SwiperSlide key={region.value} className="absolute top-1/2 -translate-y-1/3">
-              <button className={`text-base border rounded-xl w-14 sm:w-16 hover:bg-pink-50 ${selectedButtonIndex === region.value ? 'bg-gray-500 text-white font-bold' : ''}`} onClick={() => handleButtonClick(region.value)}>
+              <button
+                className={`text-sm sm:text-base whitespace-nowrap border rounded-xl w-16 sm:w-18 hover:bg-pink-50 ${selectedButtonIndex === region.value ? 'bg-gray-500 text-white font-bold' : ''}`}
+                onClick={() => handleButtonClick(region.value)}
+              >
                 {region.key}
               </button>
             </SwiperSlide>
